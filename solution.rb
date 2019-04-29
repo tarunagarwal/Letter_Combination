423310b0b8f26d7964b91f272d18a172f7618170
tarun <tarunagarwal27@gamil.com>
2019-04-29 22:18:28 +0530

    added digit to letter mapping hash.

--- solution.rb
+++ solution.rb
@@ -8,6 +8,11 @@ module Solution
     
       #return if number not valid
       return "Not a valid digit." if digits.nil? || digits.length != 10 || digits.split("").any?{|digit| digit.to_i == 0 || digit.to_i == 1}
+
+      #number to letters mapping
+      letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
+
+    
     end
   end 
 end
